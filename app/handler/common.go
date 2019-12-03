65794743125391336a3188fbab260b6d092ee154
Nathaniel Olarte <nl_olarte@yahoo.com>
2019-12-03 16:04:03 +0800

    add headers to enable CORS

--- app/handler/common.go
+++ app/handler/common.go
@@ -14,6 +14,9 @@ func respondJSON(w http.ResponseWriter, status int, payload interface{}) {
     return
   }
   w.Header().Set("Content-Type", "application/json")
+  w.Header().Set("Access-Control-Allow-Origin", "*")
+  w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+  w.Header().Set("Access-Control-Allow-Headers", "Origin, Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
   w.WriteHeader(status)
   w.Write([]byte(response))
 }
